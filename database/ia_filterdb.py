216a6a962446026a21973400e48975d628cdaaa1
Jins Mathew <jins.palakkattu@gmail.com>
2022-01-16 16:44:12 +0300

    Initial Commit

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -58,9 +58,9 @@ async def save_file(media):
             logger.warning(media.file_name + " is already saved in database")
             return False, 0
         else:
-            logger.info(media.file_name + " is saved in database")
+            # logger.info(media.file_name + " is saved in database")
+            logger.info(f'{getattr(media, "file_name", "NO_FILE")} is saved to database')
             return True, 1
-
 
 
 async def get_search_results(query, file_type=None, max_results=10, offset=0, filter=False):
