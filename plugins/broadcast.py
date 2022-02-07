26340e81586741b9350fe554f5ebfe2872b37075
DeveloperNoob <91716451+DeveloperNoob@users.noreply.github.com>
2022-02-07 20:13:23 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -223,7 +223,7 @@ __help__ = """
  
  Format For Text With Buttons:
     Hello Dear How Are You! You Can Join My Channel.
-    [Join Here](buttonurl://t.me/UFSBotz)
+    [Join Here](buttonurl://t.me/M76Links)
 """
 
 __mod_name__ = "Broadcast"
